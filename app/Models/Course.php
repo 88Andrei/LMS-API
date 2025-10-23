d3ac3192b59660940f942e8a3ed185973ef1c8d8
Andri <freeplay585@gmail.com>
2025-10-23 22:25:44 +0200

    Add Lesson model and migration for lessons associated with courses

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Course extends Model
 {
@@ -24,5 +25,10 @@ class Course extends Model
         return $this->belongsToMany(User::class)
             ->withPivot('role', 'progress')
             ->withTimestamps();   
+    }
+
+    public function lessons(): HasMany
+    {
+        return $this->hasMany(Lesson::class);
     }
 }
