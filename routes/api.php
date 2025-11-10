0ed4d355778d52913682ef092ff6b20ad9258a7e
Andri <freeplay585@gmail.com>
2025-11-10 20:12:05 +0100

    Refactor API routes to include CourseController and organize under v1 prefix

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\V1\CourseController;
 use App\Http\Controllers\Api\V1\CourseUserController;
+use App\Http\Controllers\Api\V1\LessonController;
 use App\Http\Controllers\FileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -13,3 +15,7 @@ Route::post('/courses/{course}/assign-user', [CourseUserController::class, 'assi
 
 Route::post('files/{type}/{id}/upload', [FileController::class, 'upload']);
 Route::delete('files/{file}', [FileController::class, 'destroy']);
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('courses', CourseController::class);
+});
