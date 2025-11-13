74d1d20cfd6a9d2c87970c7def7d51c171ef6a90
Andri <freeplay585@gmail.com>
2025-11-13 17:53:35 +0100

    Add LessonFactory for generating lesson test data with default attributes

---
+++ database/factories/LessonFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Lesson;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\lesson>
+ */
+class LessonFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Lesson::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'content' => $this->faker->paragraphs(3, true),
+            'course_id' => null, // To be set when creating a lesson for a specific course
+            'order' => $this->faker->numberBetween(1, 100),
+        ];
+    }
+}
