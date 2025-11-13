5fe7c415d8e3415fd8b7699bf20da455c898ff55
Andri <freeplay585@gmail.com>
2025-11-13 17:54:29 +0100

    Add authentication routes for login, registration, and logout

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\V1\CourseController;
 use App\Http\Controllers\Api\V1\CourseUserController;
 use App\Http\Controllers\Api\V1\LessonController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,6 +12,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
 Route::post('/courses/{course}/assign-user', [CourseUserController::class, 'assignUser']);
 
 Route::post('files/{type}/{id}/upload', [FileController::class, 'upload']);
@@ -18,4 +23,5 @@ Route::delete('files/{file}', [FileController::class, 'destroy']);
 
 Route::prefix('v1')->group(function () {
     Route::apiResource('courses', CourseController::class);
+    Route::apiResource('lessons', LessonController::class);
 });
